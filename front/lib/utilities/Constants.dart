cebe8e3d43f132870320401f1e31dc20742bfd4d
Carlos-MVM <mapalmarejo@gmail.com>
2023-06-24 22:25:03 -0100

    Prescription view

---
+++ front/lib/utilities/Constants.dart
@@ -0,0 +1,36 @@
+class Constants{
+  Constants._();
+  static String BLANK = '';
+
+  static String PRESCRIPTION_TITLE = 'Receita Médica';
+  static String PACIENT_PRESCRIPTION_SUBTITLE = 'Paciente';
+  static String MEDICINE_PRESCRIPTION_SUBTITLE = 'Medicamento';
+  static String DOCTOR_PRESCRIPTION_SUBTITLE = 'Médico';
+
+  static String PACIENT_PRESCRIPTION_NAME = 'Nome';
+  static String PACIENT_PRESCRIPTION_NAME_HINT = 'Pedro Almeida';
+  static String PACIENT_PRESCRIPTION_CNI = 'CNI';
+  static String PACIENT_PRESCRIPTION_CNI_HINT = '20040316M05J';
+  static String PACIENT_PRESCRIPTION_NASC = 'Data Nascimento';
+  static String PACIENT_PRESCRIPTION_NASC_HINT = '10-01-2000';
+  static String PACIENT_PRESCRIPTION_PHONE = 'Telemóvel';
+  static String PACIENT_PRESCRIPTION_PHONE_HINT = '5671234';
+
+  static String MEDICINE_PRESCRIPTION_NAME = 'Medicamento';
+  static String MEDICINE_PRESCRIPTION_NAME_HINT = 'Paracetamol';
+  static String MEDICINE_PRESCRIPTION_DOSAGE = 'Dosagem';
+  static String MEDICINE_PRESCRIPTION_DOSAGE_HINT = '10 (g)';
+  static String MEDICINE_PRESCRIPTION_AMOUNT = 'Quantidade';
+  static String MEDICINE_PRESCRIPTION_USAGE = 'Tipo de Uso';
+  static String MEDICINE_PRESCRIPTION_USAGE_HINT = 'Externo';
+  static String MEDICINE_PRESCRIPTION_ADMINISTRATION = 'Via';
+  static String MEDICINE_PRESCRIPTION_ADMINNISTRATION_HINT = 'Oral';
+  static String MEDICINE_PRESCRIPTION_DESCRIPTION = 'Descrição';
+  static String MEDICINE_PRESCRIPTION_DURATION = 'Período de Uso';
+
+  static String DOCTOR_PRESCRIPTION_CIPS = 'CIPS';
+  static String DOCTOR_PRESCRIPTION_CIPS_HINT = '030/2023';
+  static String DOCTOR_PRESCRIPTION_SIGNATURE_BUTTON = 'Assinatura Digital';
+
+  static String PRESCRIPRION_CREATE_BUTTON = 'Criar';
+}
