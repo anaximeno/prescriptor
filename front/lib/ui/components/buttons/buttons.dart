f0d2d7602766e2cca0f266d7fdd78c7790817a7a
anaximeno <anaximenobrito@gmail.com>
2023-06-25 02:06:26 -0100

    feat: abstract ui components

---
+++ front/lib/ui/components/buttons/buttons.dart
@@ -0,0 +1,3 @@
+// This file is used to export new button widgets
+//
+export 'simple_button.dart';
---
+++ front/lib/ui/components/components.dart
@@ -0,0 +1,4 @@
+// This file is used to export the components
+//
+export 'buttons/buttons.dart';
+export 'inputs/inputs.dart';
