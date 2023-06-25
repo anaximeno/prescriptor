7cb011338ca4476a29d3a494739d0cd5a33de39f
anaximeno <anaximenobrito@gmail.com>
2023-06-25 16:12:32 -0100

    feat: add user route and service

---
+++ api/src/main/java/group/three/routes/UserRoute.java
@@ -0,0 +1,61 @@
+package group.three.routes;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Produces;
+
+import group.three.services.UserService;
+
+@Path("/api/user")
+@ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
+public class UserRoute {
+    @Inject
+    UserService userService;
+
+    @GET
+    @Path("/")
+    public Response show() {
+        return userService.getUsers();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response index(@PathParam("id") Long id) {
+        return userService.getUserById(id);
+    }
+
+    @POST
+    @Path("/")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response store() {
+        return userService.storeUser();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update() {
+        return userService.updateUser();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        return userService.deleteUserById(id);
+    }
+}
