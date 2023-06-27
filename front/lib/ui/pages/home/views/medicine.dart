293996f3db76d0b428a41f1b44af2b3c7c4dbb85
Carlos-MVM <mapalmarejo@gmail.com>
2023-06-27 11:38:33 -0100

    Add class: Medicine

---
+++ front/lib/ui/pages/home/views/medicine.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:front/ui/components/buttons/big_button.dart';
+import 'package:front/ui/components/texts/title_text.dart';
+import 'package:front/utils/constants.dart';
+
+class Medicine extends StatelessWidget {
+  const Medicine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          Container(padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Expanded(child: Container(child: TitleText(title: Constants.DASHBOARD, color: Colors.black))),
+
+                BigButton(text: Constants.MEDICINE_SEARCH, buttonColor: Colors.lightBlueAccent, textColor: Colors.white),
+              ],
+            ),
+          ),
+          Expanded(child: Container(color: Colors.lightBlueAccent,
+              child: Placeholder() // If press create prescription
+          )),
+        ]);
+  }
+}
