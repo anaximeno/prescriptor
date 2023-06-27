eb06edaf4045bf7d3e80222044215dcf3d88b92f
anaximeno <anaximenobrito@gmail.com>
2023-06-27 22:54:22 -0100

    feat: add new repositories and update others

--- front/lib/domain/repositories/user.dart
+++ front/lib/domain/repositories/user.dart
@@ -6,7 +6,7 @@ import 'package:front/infra/api.dart';
 class UserRepository {
   final Api _api = Api(); // XXX: use dep injection
 
-  Future<UserEntity?> getById(int id) async {
+  Future<UserEntity> getById(int id) async {
     try {
       final response = await _api.get('user/$id');
       final data = jsonDecode(response.body)['data'];
@@ -14,5 +14,49 @@ class UserRepository {
     } catch (e) {
       rethrow;
     }
+  }
+
+  Future<List<UserEntity>> getAll() async {
+    try {
+      final response = await _api.get('user');
+      final data = jsonDecode(response.body)['data'];
+      return List.from(data)
+          .map((e) => UserEntity.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<UserEntity?> storeUser(
+    UserEntity entity,
+  ) async {
+    try {
+      final response = await _api.post(
+        'user',
+        body: entity.toJson(),
+      );
+      final data = jsonDecode(response.body)['data'];
+      return UserEntity.fromJson(data as Map<String, dynamic>);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<UserEntity> updateUser(
+    int id,
+    UserEntity entity,
+  ) async {
+    try {
+      final response = await _api.put('user/$id');
+      final data = jsonDecode(response.body)['data'];
+      return UserEntity.fromJson(data as Map<String, dynamic>);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> deleteUser(int id) async {
+    await _api.delete('user/$id');
   }
 }
