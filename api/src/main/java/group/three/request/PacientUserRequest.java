ba9405f97b04700d91823470dd4329e1ba45693c
anaximeno <anaximenobrito@gmail.com>
2023-06-27 01:17:52 -0100

    chore: ensure pacient sets the correct user type

--- api/src/main/java/group/three/request/PacientUserRequest.java
+++ api/src/main/java/group/three/request/PacientUserRequest.java
@@ -2,7 +2,7 @@ package group.three.request;
 
 import group.three.model.Pacient;
 import group.three.model.User;
-
+import group.three.utils.enums.UserType;
 import lombok.*;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -13,10 +13,17 @@ import lombok.*;
 public class PacientUserRequest extends UserRequest {
     private Boolean hasInsurance;
 
+    @Override
+    public User toUserEntity() {
+        final User user = super.toUserEntity();
+        user.setUserType(UserType.PACIENT);
+        return user;
+    }
+
     public Pacient toPacientEntity(User user) {
         return Pacient.builder()
-            .hasInsurance(hasInsurance)
-            .user(user)
-            .build();
+                .hasInsurance(hasInsurance)
+                .user(user)
+                .build();
     }
 }
