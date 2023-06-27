c36bbb276c3d4b41dfc47ad66b4e9bd7851a173a
anaximeno <anaximenobrito@gmail.com>
2023-06-27 13:32:47 -0100

    fix: update api infra for front

--- front/lib/infra/api.dart
+++ front/lib/infra/api.dart
@@ -1,26 +1,28 @@
 import 'package:http/http.dart' as http;
 
 class Api {
-  final String baseRoute;
+  final String hostAddress;
+  final String apiRoute;
 
-  Api({this.baseRoute = "localhost:8182/api"});
+  Api({this.hostAddress = "localhost:8182", this.apiRoute = "/api"});
 
-  Uri _url(String route, {Map<String, dynamic>? params}) =>
-      Uri.http(baseRoute, route, params);
-
-  Future<http.Response> post(String route, {Map<String, dynamic>? body}) {
-    return http.post(_url(route), body: body);
+  Uri _url(String resource, {Map<String, dynamic>? params}) {
+    return Uri.http(hostAddress, '$apiRoute/$resource', params);
   }
 
-  Future<http.Response> put(String route, {Map<String, dynamic>? body}) {
-    return http.put(_url(route), body: body);
+  Future<http.Response> post(String resource, {Map<String, dynamic>? body}) {
+    return http.post(_url(resource), body: body);
   }
 
-  Future<http.Response> get(String route, {Map<String, dynamic>? params}) {
-    return http.get(_url(route, params: params));
+  Future<http.Response> put(String resource, {Map<String, dynamic>? body}) {
+    return http.put(_url(resource), body: body);
   }
 
-  Future<http.Response> delete(String route) {
-    return http.delete(_url(route));
+  Future<http.Response> get(String resource, {Map<String, dynamic>? params}) {
+    return http.get(_url(resource, params: params));
+  }
+
+  Future<http.Response> delete(String resource) {
+    return http.delete(_url(resource));
   }
 }
