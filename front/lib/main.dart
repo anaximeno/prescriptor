3a32a6be36420fd3196b0f63b85f648dc457128f
anaximeno <anaximenobrito@gmail.com>
2023-06-28 00:32:14 -0100

    feat: add get for state management

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 import 'package:front/ui/pages/home/home_page.dart';
 import 'package:front/ui/pages/login/login_page.dart';
+import 'package:front/utils/utils.dart';
+
 import './ui/theming.dart';
 
 void main() => runApp(const App());
@@ -10,15 +14,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Prescriptor',
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Theming.appTheme,
+      title: Constants.APPBAR_TITLE,
+      initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
         '/': (context) => const HomePage(),
       },
-      initialRoute: '/login',
-      debugShowCheckedModeBanner: false,
-      theme: Theming.appTheme,
     );
   }
 }
