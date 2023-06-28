b179211d76944a5b37ea6039e35f253df8735af2
anaximeno <anaximenobrito@gmail.com>
2023-06-28 01:06:06 -0100

    chore: update simple button

--- front/lib/ui/components/buttons/simple_button.dart
+++ front/lib/ui/components/buttons/simple_button.dart
@@ -3,35 +3,41 @@ import 'package:flutter/material.dart';
 class SimpleButton extends StatelessWidget {
   final String text;
   final double width;
-  final Color backgroundColor;
+  final double height;
+  final Color enabledColor;
+  final Color disabledColor;
+
   final void Function()? onPressed;
 
   const SimpleButton({
     super.key,
     required this.text,
     required this.width,
-    this.backgroundColor = const Color(0xFF7FB8D6),
     this.onPressed,
+    this.height = 35,
+    this.enabledColor = const Color(0xFF0DA0F0),
+    this.disabledColor = const Color(0xFF0DA0F0),
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(padding: EdgeInsets.all(8.0),
-      width: width,
-      height: 35,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: backgroundColor,
-      ),
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: null,
-          //minimumSize: const Size(80, 30),
-          elevation: 10
+    return Container(
+        padding: const EdgeInsets.all(8.0),
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: onPressed != null ? enabledColor : disabledColor,
         ),
-        child: Text(text, style: const TextStyle(color: Colors.white, fontSize: 15)),
-      ),
-    );
+        child: TextButton(
+          onPressed: onPressed,
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
+          ),
+        ));
   }
 }
